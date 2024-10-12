a9318b743c9c2f4ddbd55d9e982db705d28d9e12
YanilsonMR <myanilson@gmail.com>
2024-10-12 00:43:38 -0500

    Listas terminada y menu actualizado

--- Metodos.java
+++ Metodos.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class Metodos {
 
     Scanner sc = new Scanner(System.in);
+    int opt = 0;
 
     public LinkedList<EstudianteIngenieria> LLenarListaEstudianteING() {
 
-        boolean continuar = true;
-        int opt = 0;
+        boolean continuar = true;        
         LinkedList<EstudianteIngenieria> Lista = new LinkedList<>();
         String Nombre;
         String Cedula;
@@ -74,8 +74,7 @@ public class Metodos {
 
     public LinkedList<EstudianteDiseño> LLenarListaEstudianteDIS(){
 
-        boolean continuar = true;
-        int opt = 0;
+        boolean continuar = true;        
         LinkedList<EstudianteDiseño> Lista = new LinkedList<>();
         String Cedula;
         String Nombre;
@@ -138,7 +137,122 @@ public class Metodos {
 
     }
 
-    
+    public LinkedList<TabletaGrafica> LLenarListaTableta(){
+        LinkedList<TabletaGrafica> Lista = new LinkedList<>();
+        boolean continuar = true;
+        String Serial;
+        String Marca;
+        float Tamaño;
+        float Precio;
+        int Almacenamiento;
+        float Peso;
 
+            while (continuar) {
+                System.out.println("==================================");
+                System.out.println("=== Llenando lista de tabletas ===");
+                System.out.println(" Serial de la tableta");
+                Serial = sc.next();
+                System.out.println("Marca de la tableta");
+                Marca = sc.next();
+                System.out.println(" Tamaño de la tablet ");
+                Tamaño = sc.nextFloat();
+                System.out.println("Dame el precio de la tableta");
+                Precio = sc.nextFloat();
+                System.out.println("Almacenamiento de la tableta");
+                Almacenamiento = sc.nextInt();
+                System.out.println(" Cuanto pesa la tableta?");
+                Peso = sc.nextFloat();
+                System.out.println();
+                TabletaGrafica e = new TabletaGrafica(Serial, Marca, Tamaño, Precio, Almacenamiento, Peso);
+                Lista.add(e);
+                System.out.println(" Quieres agregar mas tabletas a la lista ?");
+                opt = sc.nextInt();
+                while (opt <= 0 || opt > 2) {
+                    System.out.println("opcion incorrecta");
+                    System.out.println("¿desea agregar mas computadores a la lista? 1: SI, 2: NO ");
+                    while (!sc.hasNextInt()) {
+                        System.out.println("ingrese una opcion valida");
+                    }
+                    opt = sc.nextInt();
+                }
+                if (opt == 2) {
+                    continuar = false;
+                }
+            }
+        return Lista;
+    }
+
+    public void MostrarlistaTABLET(LinkedList<TabletaGrafica> Lista){
+
+        for(TabletaGrafica TabletaGrafica : Lista){
+            System.out.println("========================================");
+            System.out.println("Serial: " + TabletaGrafica.getSerial());
+            System.out.println("Marca: " + TabletaGrafica.getMarca());
+            System.out.println("Tamaño: " + TabletaGrafica.getTamaño());
+            System.out.println("Precio: " + TabletaGrafica.getPrecio());
+            System.out.println("Alamacenamiento: " + TabletaGrafica.getAlmacenamiento());
+            System.out.println("Peso: " + TabletaGrafica.getPeso());
+        }
+
+    }
+
+    public LinkedList<ComputadorPortatil> LLenarListaComputadores(){
+        LinkedList<ComputadorPortatil> Lista = new LinkedList<>();
+        boolean continuar = true;
+        String Serial;
+        String Marca;
+        Float Tamaño;
+        float Precio;
+        String So;
+        String Procesador;
+            while (continuar) {
+                System.out.println("======================================");
+                System.out.println("=== Llenando lista de computadores === ");
+                System.out.println("Dime el serial del computador");
+                Serial = sc.next();
+                System.out.println(" Dime la marca del computador");
+                Marca = sc.next();
+                System.out.println("Que tamaño tiene la pantalla del compurador (Recuerda que son pulgadas)");
+                Tamaño = sc.nextFloat();
+                System.out.println("DIme el precio del computador");
+                Precio = sc.nextFloat();
+                System.out.println("DImke el sistema operativo del computador");
+                So = sc.next();
+                System.out.println("DIme el procesador del computador");
+                Procesador = sc.next();
+                System.out.println();
+                ComputadorPortatil e = new ComputadorPortatil(Serial, Marca, Tamaño, Precio, So, Procesador);
+                Lista.add(e);
+                System.out.println(" Quieres agregar mas computadores a la lista ?");
+                opt = sc.nextInt();
+                while (opt <= 0 || opt > 2) {
+                    System.out.println("opcion incorrecta");
+                    System.out.println("¿desea agregar mas computadores a la lista? 1: SI, 2: NO ");
+                    while (!sc.hasNextInt()) {
+                        System.out.println("ingrese una opcion valida");
+                    }
+                    opt = sc.nextInt();
+                }
+                if (opt == 2) {
+                    continuar = false;
+                }
+
+        }
+        return Lista;
+    }
+
+    public void MostrarlistaCOMPUTADORES(LinkedList<ComputadorPortatil> Lista){
+
+        for(ComputadorPortatil Computador : Lista){
+            System.out.println("====================");
+            System.out.println("Serial " + Computador.getSerial());
+            System.out.println("Marca " + Computador.getMarca());
+            System.out.println("Tamaño " + Computador.getTamaño());
+            System.out.println("Precio " + Computador.getPrecio());
+            System.out.println("Sistema OPerativo " + Computador.getSo());
+            System.out.println("Procesador " + Computador.getProcesador());
+        }
+
+    }
 
 }
--- Principal.java
+++ Principal.java
@@ -1,13 +1,16 @@
 import java.util.LinkedList;
 import java.util.Scanner;
 public class Principal {
-    public LinkedList<ComputadorPortatil> vectorComputador = new LinkedList<>();
-    public LinkedList<TabletaGrafica> vectorTableta = new LinkedList<>();
-    public LinkedList<EstudianteDiseño> vectorDIS = new LinkedList<>();
-    public LinkedList<EstudianteIngenieria> vectorING = new LinkedList<>();
     public static void main(String[] args) {
-        int opt;
-        Scanner sc = new Scanner(System.in);
+    LinkedList<ComputadorPortatil> listaComputador = new LinkedList<>();
+    LinkedList<TabletaGrafica> listaTableta = new LinkedList<>();
+    LinkedList<EstudianteDiseño> listaDIS = new LinkedList<>();
+    LinkedList<EstudianteIngenieria> listaING = new LinkedList<>();
+    Metodos m = new Metodos();
+    boolean bandera = true;
+    int opt;
+    Scanner sc = new Scanner(System.in);
+        while (bandera) {
             System.out.println("----- MENÚ PRINCIPAL -----");
             System.out.println("1. INGRESAR ESTUDIANTES DE INGENIERÍA A LA LISTA");
             System.out.println("2. VER LISTA DE ESTUDIANTES DE INGENIERIA");
@@ -17,31 +20,44 @@ public class Principal {
             System.out.println("5. VER LSITA DE COMPUTADORES");
             System.out.println("7. INGRESAR NUEVAS TABLETAS");
             System.out.println("8. VER LISTA DE TABLETAS");
-            System.out.println("5. SALIR");
+            System.out.println("9. SALIR");
             System.out.print("Seleccione una opción: ");
             opt = sc.nextInt();
 
             switch (opt) {
                 case 1:
-                    LLenarListaEstudianteING();
+                    listaING = m.LLenarListaEstudianteING();                    
                     break;
                 case 2:
-                    
+                    m.MostrarlistaING(listaING);
                     break;
                 case 3:
-                    
+                    listaDIS = m.LLenarListaEstudianteDIS();
                     break;
                 case 4:
+                    m.MostrarlistaDIS(listaDIS);
+                    break;
+                case 5:
+                    listaComputador = m.LLenarListaComputadores();              
+                    break;
+                case 6:
+                    m.MostrarlistaCOMPUTADORES(listaComputador);
+                    break;
+                case 7:
+                    listaTableta = m.LLenarListaTableta();
+                    break;
+                case 8:
+                    m.LLenarListaTableta();
+                    break;
+                case 9:
                     System.out.println("Saliendo del programa...");
                     break;
+
                 default:
                     System.out.println("Opción no válida, intente de nuevo.");
             }
-        
-    }
-    public static void LLenarListaEstudianteING() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'LLenarListaEstudianteING'");
+        }
+                   
     }
 
 }
