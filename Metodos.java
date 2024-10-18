33e4b8c15017b6dc3335878aee27671bcb560d70
YanilsonMR <myanilson@gmail.com>
2024-10-18 15:59:34 -0500

    Metodos de exportar y listas de estudianstes agregadas

--- Metodos.java
+++ Metodos.java
@@ -1,3 +1,8 @@
+
+//import java.io.BufferedReader;
+//import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -295,6 +300,119 @@ public class Metodos {
             System.out.println("Precio " + Computador.getPrecio() + "$");
             System.out.println("Sistema OPerativo " + Computador.getSo());
             System.out.println("Procesador " + Computador.getProcesador());
+        }
+    }
+
+    public void exportarING(LinkedList<EstudianteIngenieria> lista) {
+
+        LinkedList<EstudianteIngenieria> listaING = new LinkedList<>();
+
+        if (listaING.isEmpty()) {
+            Metodos m = new Metodos();
+            listaING = m.LLenarListaEstudianteING();
+        } else {
+            try (FileWriter escriba = new FileWriter("LIstadeING.txt")) {
+                for (EstudianteIngenieria estudiante : lista) {
+                    escriba.write("Nombre: " + estudiante.getNombre() + "\n");
+                    escriba.write("Cedula: " + estudiante.getCedula() + "\n");
+                    escriba.write("Apellido: " + estudiante.getApellido() + "\n");
+                    escriba.write("Telefono: " + estudiante.getTelefono() + "\n");
+                    escriba.write("Semestre: " + estudiante.getNumerosemestre() + "\n");
+                    escriba.write("Promedio: " + estudiante.getPromedio() + "\n");
+                    escriba.write("Serial: " + estudiante.getSerial() + "\n");
+                    escriba.write("---------------------------------------\n");
+
+                }
+
+                System.out.println("LIsta de estudiantes de ingenieria exportado correctamente ...");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void exportarDIS(LinkedList<EstudianteDiseño> lista) {
+
+        LinkedList<EstudianteDiseño> listaDIS = new LinkedList<>();
+
+        if (listaDIS.isEmpty()) {
+            Metodos m = new Metodos();
+            listaDIS = m.LLenarListaEstudianteDIS();
+        } else {
+            try (FileWriter escriba = new FileWriter("LIstadeDIS.txt")) {
+                for (EstudianteDiseño estudianted : lista) {
+                    escriba.write("Nombre: " + estudianted.getNombre() + "\n");
+                    escriba.write("Apellido: " + estudianted.getApellido() + "\n");
+                    escriba.write("Telefono " + estudianted.getTelefono() + "\n");
+                    escriba.write("Modalida de studio " + estudianted.getModalidadestudio() + "\n");
+                    escriba.write("Cantidad de materias: " + estudianted.getCantasignatura() + "\n");
+                    escriba.write("Serial: " + estudianted.getSerial() + "\n");
+                    escriba.write("---------------------------------------\n");
+
+                }
+
+                System.out.println("LIsta de estudiantes de diseño ha sido exportada correctamente ...");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void exportarPC(LinkedList<ComputadorPortatil> lista) {
+
+        LinkedList<ComputadorPortatil> listaComputador = new LinkedList<>();
+
+        if (listaComputador.isEmpty()) {
+            Metodos m = new Metodos();
+            System.out.println("las lista de computadores esta vacia");
+            System.out.println("porfavor llenar la lista con los computadores disponibles");
+
+            listaComputador = m.LLenarListaComputadores();
+        } else {
+            try (FileWriter escriba = new FileWriter("ListaPortatiles.txt")) {
+                for (ComputadorPortatil computador : lista) {
+                    escriba.write("serial: " + computador.getSerial() + "\n");
+                    escriba.write("Marca: " + computador.getMarca() + "\n");
+                    escriba.write("Tamaño: " + computador.getTamaño() + "\n");
+                    escriba.write("Precio: " + computador.getPrecio() + "\n");
+                    escriba.write("Sistema operativo: " + computador.getSo() + "\n");
+                    escriba.write("Procesador: " + computador.getProcesador() + "\n");
+                    escriba.write("---------------------------------------\n");
+
+                }
+
+                System.out.println("LIsta de computadores ha sido exportado correctamente ...");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void exportarTABLETA(LinkedList<TabletaGrafica> lista) {
+
+        LinkedList<TabletaGrafica> listaTABLETA = new LinkedList<>();
+
+        if (listaTABLETA.isEmpty()) {
+            Metodos m = new Metodos();
+            listaTABLETA = m.LLenarListaTableta();
+        } else {
+            try (FileWriter escriba = new FileWriter("ListaTabletas.txt")) {
+                for (TabletaGrafica tableta : lista) {
+                    escriba.write("Serial: " + tableta.getSerial() + "\n");
+                    escriba.write("Marca: " + tableta.getMarca() + "\n");
+                    escriba.write("Tamaño: " + tableta.getTamaño() + "\n");
+                    escriba.write("Precio: " + tableta.getPrecio() + "\n");
+                    escriba.write("Almacenamiento: " + tableta.getAlmacenamiento() + "\n");
+                    escriba.write("Peso: " + tableta.getPeso() + "\n");
+                    escriba.write("---------------------------------------\n");
+
+                }
+
+                System.out.println("LIsta de tabletas ha sido exportado correctamente ...");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
     }
--- Principal.java
+++ Principal.java
@@ -1,64 +1,128 @@
 import java.util.LinkedList;
 import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
-    LinkedList<ComputadorPortatil> listaComputador = new LinkedList<>();
-    LinkedList<EstudianteDiseño> listaDIS = new LinkedList<>();
-    LinkedList<EstudianteIngenieria> listaING = new LinkedList<>();
-    LinkedList<TabletaGrafica> listatableta = new LinkedList<>();
-    Metodos m = new Metodos();
-    boolean bandera = true;
-    int opt;
-    Scanner sc = new Scanner(System.in);
+        LinkedList<ComputadorPortatil> listaComputador = new LinkedList<>();
+        LinkedList<EstudianteDiseño> listaDIS = new LinkedList<>();
+        LinkedList<EstudianteIngenieria> listaING = new LinkedList<>();
+        LinkedList<TabletaGrafica> listatableta = new LinkedList<>();
+        Metodos m = new Metodos();
+        boolean bandera = true;
+        int opt;
+        Scanner sc = new Scanner(System.in);
         while (bandera) {
             System.out.println("----- MENÚ PRINCIPAL -----");
-            System.out.println("1. INGRESAR ESTUDIANTES DE INGENIERÍA A LA LISTA");
-            System.out.println("2. VER LISTA DE ESTUDIANTES DE INGENIERIA");
-            System.out.println("3. INGRESAR ESTUDIANTES DE DISEÑO A LA LISTA");
-            System.out.println("4. VER LISTA DE ESTUDIANTES DE DISEÑO");
-            System.out.println("5. INGRESAR NUEVOS COMPUTADORES");
-            System.out.println("5. VER LSITA DE COMPUTADORES");
-            System.out.println("7. INGRESAR NUEVAS TABLETAS");
-            System.out.println("8. VER LISTA DE TABLETAS");
-            System.out.println("9. SALIR");
+            System.out.println("1.  LLENAR LAS LISTAS");
+            System.out.println("2.  VER LAS LISTAS");
+            System.out.println("3.  EXPORTAR LAS LISTAS");
+            System.out.println("4.  IMPORTAR LAS LISTAS");
             System.out.print("Seleccione una opción: ");
             opt = sc.nextInt();
-
             switch (opt) {
                 case 1:
-                    listaING = m.LLenarListaEstudianteING();                    
+                    System.out.println("AQUI PUEDES LLENAR DIFERENTES LISTAS DE: ");
+                    System.out.println("1. LLENAR LISTA DE ESTUDIANTES DE INGENIERIA");
+                    System.out.println("2. LLENAR LISTA DE ESTUDIANTES DE DISEÑO");
+                    System.out.println("3. LLENAR LSITA DE COMPUTADORES");
+                    System.out.println("4. LLENAR LISTA DE TABLETAS");
+                    System.out.println("");
+                    opt = sc.nextInt();
+                    switch (opt) {
+                        case 1:
+                            listaING = m.LLenarListaEstudianteING();
+                            break;
+                        case 2:
+                            listaDIS = m.LLenarListaEstudianteDIS();
+                            break;
+                        case 3:
+                            listaComputador = m.LLenarListaComputadores();
+                            break;
+                        case 4:
+                            listatableta = m.LLenarListaTableta();
+                            break;
+
+                        default:
+                            System.out.println("Opción no válida, intente de nuevo.");
+                            break;
+                    }
                     break;
                 case 2:
-                    m.MostrarlistaING(listaING);
+                    System.out.println("AQUI PUEDES VER DIFERENTES LISTAS: ");
+                    System.out.println("1. VER LISTA DE ESTUDIANTES DE INGENIERIA");
+                    System.out.println("2. VER LISTA DE ESTUDIANTES DE DISEÑO");
+                    System.out.println("3. VER LSITA DE COMPUTADORES");
+                    System.out.println("4. VER LISTA DE TABLETAS");
+                    System.out.println("5. SALIR DEL PROGRAMA");
+                    System.out.println("");
+                    opt = sc.nextInt();
+                    switch (opt) {
+                        case 1:
+                            m.MostrarlistaING(listaING);
+                            break;
+                        case 2:
+                            m.MostrarlistaDIS(listaDIS);
+                            break;
+                        case 3:
+                            m.MostrarlistaCOMPUTADORES(listaComputador);
+                            break;
+                        case 4:
+                            m.MostrarlistaTABLET(listatableta);
+                            break;
+
+                        default:
+                            System.out.println("Opción no válida, intente de nuevo.");
+                            break;
+                    }
+
                     break;
                 case 3:
-                    listaDIS = m.LLenarListaEstudianteDIS();
+                    System.out.println("aqui podras exportar las diferentes listas");
+                    System.out.println("1. EXPORTAR LA LISTA DE ESTUDIANTES DE INGENIERIA");
+                    System.out.println("2. EXPORTAR LISTA DE ESTUDIANTES DE DISEÑO");
+                    System.out.println("3. EXPORTAR LISTA DE COMPUTADORES");
+                    System.out.println("4. EXPORTAR LISTA DE TABLETAS");
+                    System.out.println("5. EXPORTAR TODAS LAS LISTAS");
+                    opt = sc.nextInt();
+                    switch (opt) {
+                        case 1:
+                            m.exportarING(listaING);
+                            break;
+                        case 2:
+                            m.exportarDIS(listaDIS);
+                            break;
+                        case 3:
+                            m.exportarPC(listaComputador);
+                            break;
+                        case 4:
+                            m.exportarTABLETA(listatableta);
+                            break;
+                        case 5:
+                            m.exportarING(listaING);
+                            m.exportarDIS(listaDIS);
+                            m.exportarPC(listaComputador);
+                            m.exportarTABLETA(listatableta);
+                        break;
+
+                        default:
+                            System.out.println("Opcion no validad. Intenta de nuevo. ");
+                            break;
+                    }
                     break;
                 case 4:
-                    m.MostrarlistaDIS(listaDIS);
+
                     break;
                 case 5:
-                    listaComputador = m.LLenarListaComputadores();              
-                    break;
-                case 6:
-                    m.MostrarlistaCOMPUTADORES(listaComputador);
-                    break;
-                case 7:
-                    listatableta = m.LLenarListaTableta();
-                    break;
-                case 8:
-                    m.MostrarlistaTABLET(listatableta);
-                    break;
-                case 9:
                     System.out.println("Saliendo del programa...");
                     bandera = false;
                     break;
 
                 default:
                     System.out.println("Opción no válida, intente de nuevo.");
+                    break;
             }
         }
-        sc.close();         
+        sc.close();
     }
 
 }
